f6ba67c7a6c78e29a4232ece0f811cdaebc4ca5d
Stephan <stephan.iskander@gmail.com>
2026-09-22 22:02:36 -0400

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        res = set()
+        for i in range(len(nums)):
+            seen = set()
+            for j in range(i+1, len(nums)):
+                compliment = (-nums[i] - nums[j])
+                if compliment in seen:
+                    res.add(tuple((sorted((nums[i], nums[j], compliment)))))
+                seen.add(nums[j])
+        return [list(x) for x in res] 
